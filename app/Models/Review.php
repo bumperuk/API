33a2c5afef929397453d1e575d21c05402a41131
Tom Lerendu <tlr@dreamr.uk>
2017-01-04 11:03:17 +0000

    Relationships on review model

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -5,5 +5,17 @@ namespace App\Models;
 
 class Review extends BaseModel
 {
-    //
+    protected $casts = [
+        'rating' => 'integer',
+    ];
+
+    public function reviewer()
+    {
+        return $this->hasOne(User::class, 'reviewer_id');
+    }
+
+    public function reviewed()
+    {
+        return $this->hasOne(User::class, 'reviewed_id');
+    }
 }
