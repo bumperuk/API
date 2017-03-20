f11343345d5df1d82b512373c3d46940f8ee077a
Tom Lerendu <tlr@dreamr.uk>
2017-03-20 11:36:47 +0000

    Hide receipt

--- app/Models/User.php
+++ app/Models/User.php
@@ -34,7 +34,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token', 'dealerRank', 'dealer_rank'
+        'password', 'remember_token', 'dealerRank', 'dealer_rank', 'receipt'
     ];
 
     /**
