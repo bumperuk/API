601bccd2d9e0feba32f5d1fc01f959b1d623fd40
Tom Lerendu <tom@tomlerendu.uk>
2017-05-23 14:50:41 +0100

    Fix clear search bug

--- resources/views/admin/layout/search.blade.php
+++ resources/views/admin/layout/search.blade.php
@@ -1,3 +1,7 @@
 <form>
     <input type="text" class="form-control" name="q" value="{{ Request::input('q') }}" placeholder="{{ $placeholder or 'Search' }}">
 </form>
+@if (Request::has('q'))
+    <a href="{{ Request::url() }}" class="pull-right">Clear Search</a>
+    <br><br>
+@endif
