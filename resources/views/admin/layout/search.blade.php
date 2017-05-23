2e90b8ebb10a9ff1da0fd1a541684f244f6a3e03
Tom Lerendu <tom@tomlerendu.uk>
2017-05-23 15:25:30 +0100

    Search by anything

--- resources/views/admin/layout/search.blade.php
+++ resources/views/admin/layout/search.blade.php
@@ -1,5 +1,5 @@
 <form>
-    <input type="text" class="form-control" name="q" value="{{ Request::input('q') }}" placeholder="{{ $placeholder or 'Search' }}">
+    <input type="text" class="form-control" size="30" name="q" value="{{ Request::input('q') }}" placeholder="{{ $placeholder or 'Search' }}">
 </form>
 @if (Request::has('q'))
     <a href="{{ Request::url() }}" class="pull-right">Clear Search</a>
--- resources/views/admin/vehicles/index.blade.php
+++ resources/views/admin/vehicles/index.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Listings')
 
 @section('header')
-    @include('admin.layout.search', ['placeholder' => 'Search by user'])
+    @include('admin.layout.search', ['placeholder' => 'Search by make, model or user'])
 @endsection
 
 @section('content')
