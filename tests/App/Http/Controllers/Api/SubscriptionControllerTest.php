78667e05037fea949c81fdc07e7e2f31a23bfef6
Tom Lerendu <tlr@dreamr.uk>
2017-08-21 09:25:22 +0100

    Subscriptions endpoint

--- tests/App/Http/Controllers/Api/SubscriptionControllerTest.php
+++ tests/App/Http/Controllers/Api/SubscriptionControllerTest.php
@@ -2,15 +2,17 @@
 
 class SubscriptionControllerTest extends TestCase
 {
-    public function testViewingIos()
+    public function testViewingSubscriptionsIos()
     {
         factory(\App\Models\DealerRank::class)->create(['name' => 'com.dealer-rank-1', 'platform' => 'ios', 'active' => true, 'limit' => 44]);
         factory(\App\Models\DealerRank::class)->create(['name' => 'com.dealer-rank-2', 'platform' => 'ios', 'active' => true, 'limit' => 22]);
-        factory(\App\Models\DealerRank::class)->create(['name' => 'com.dealer-rank-3', 'platform' => 'android', 'active' => true, 'limit' => 22]);
-        factory(\App\Models\DealerRank::class)->create(['name' => 'com.dealer-rank-1', 'platform' => 'ios', 'active' => false, 'limit' => 11]);
+        factory(\App\Models\DealerRank::class)->create(['name' => 'com.dealer-rank-3', 'platform' => 'android', 'active' => true, 'limit' => 33]);
+        factory(\App\Models\DealerRank::class)->create(['name' => 'com.dealer-rank-4', 'platform' => 'ios', 'active' => false, 'limit' => 11]);
+
+        $user = factory(\App\Models\User::class)->create(['dealer_rank_id' => null]);
 
         $this
-            ->withNewToken()
+            ->withToken($user)
             ->apiCall('GET', 'api/v1/subscriptions?platform=ios')
             ->seeSuccess()
             ->seePayloadStructure([
@@ -22,9 +24,37 @@ class SubscriptionControllerTest extends TestCase
         $body = $this->decodeResponseJson()['response_payload']['data'];
 
         $this->assertEquals(2, count($body));
-        $this->assertEquals('com.dealer-rank-2', $body[0]['name']);
-        $this->assertEquals('com.dealer-rank-1', $body[1]['name']);
-        $this->dontSeeJson(['name' => 'com.dealer-rank-3']);
-        $this->dontSeeJson(['name' => 'com.dealer-rank-4']);
+        $this->assertEquals('com.dealer-rank-2', $body[0]['id']);
+        $this->assertEquals('com.dealer-rank-1', $body[1]['id']);
+        $this->dontSeeJson(['id' => 'com.dealer-rank-3']);
+        $this->dontSeeJson(['id' => 'com.dealer-rank-4']);
+    }
+
+    public function testViewingSubscriptionsAndroid()
+    {
+        factory(\App\Models\DealerRank::class)->create(['name' => 'com.dealer-rank-1', 'platform' => 'android', 'active' => true, 'limit' => 44]);
+        factory(\App\Models\DealerRank::class)->create(['name' => 'com.dealer-rank-2', 'platform' => 'ios', 'active' => true, 'limit' => 22]);
+        factory(\App\Models\DealerRank::class)->create(['name' => 'com.dealer-rank-3', 'platform' => 'ios', 'active' => true, 'limit' => 33]);
+        factory(\App\Models\DealerRank::class)->create(['name' => 'com.dealer-rank-4', 'platform' => 'android', 'active' => false, 'limit' => 11]);
+
+        $user = factory(\App\Models\User::class)->create(['dealer_rank_id' => null]);
+
+        $this
+            ->withToken($user)
+            ->apiCall('GET', 'api/v1/subscriptions?platform=android')
+            ->seeSuccess()
+            ->seePayloadStructure([
+                '*' => [
+                    'id', 'active'
+                ]
+            ]);
+
+        $body = $this->decodeResponseJson()['response_payload']['data'];
+
+        $this->assertEquals(1, count($body));
+        $this->assertEquals('com.dealer-rank-1', $body[0]['id']);
+        $this->dontSeeJson(['id' => 'com.dealer-rank-3']);
+        $this->dontSeeJson(['id' => 'com.dealer-rank-2']);
+        $this->dontSeeJson(['id' => 'com.dealer-rank-4']);
     }
 }
