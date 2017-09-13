4fe7b8326757755c66c9dddf82d6e71a7b37517e
Tom <tlr@dreamr.uk>
2017-09-13 13:21:45 +0100

    Fix constraints

---
+++ database/migrations/2017_09_13_131303_cascade_favourites_on_vehicle_delete.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CascadeFavouritesOnVehicleDelete extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('favourites', function (Blueprint $table) {
+            $table->dropForeign(['vehicle_id']);
+            $table->foreign('vehicle_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->dropForeign(['user_id']);
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('favourites', function (Blueprint $table) {
+            $table->dropForeign(['vehicle_id']);
+            $table->foreign('vehicle_id')->references('id')->on('vehicles')->onUpdate('restrict')->onDelete('restrict');
+            $table->dropForeign(['user_id']);
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('restrict')->onDelete('restrict');
+        });
+    }
+}
